144d788cfc5c44b3fd3bd360ad034bf59cdbb88d
hkooost <djzimniy@gmail.com>
2025-01-31 12:15:36 +0300

    feat: add mobx

---
+++ src/store/store.ts
@@ -0,0 +1,76 @@
+import axios from 'axios';
+import { makeAutoObservable, runInAction } from 'mobx';
+
+const BASE_API: string = 'https://nekos.best/api/v2/waifu?amount=3';
+const BASE_DATA: string = 'https://fakerapi.it/api/v2/persons?_quantity=3&_gender=female&_birthday_start=1999-01-01&_birthday_end=2003-01-01';
+
+interface User {
+  id: number;
+  firstname: string;
+  address: {
+    country: string;
+    city: string;
+  };
+}
+
+interface Image {
+  url: string;
+}
+
+interface ImageResponse {
+  results: Image[];
+}
+
+interface UserResponse {
+  data: User[];
+}
+
+export interface CombinedData {
+  id: number;
+  firstname: string;
+  country: string;
+  city: string;
+  imageUrl: string;
+}
+
+class CardDeckStore {
+  combinedData: CombinedData[] = [];
+  loading: boolean = true;
+  error: string | null = null;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  fetchData = async () => {
+    try {
+      const [userRes, imageRes] = await Promise.all([
+        axios.get<UserResponse>(BASE_DATA),
+        axios.get<ImageResponse>(BASE_API)
+      ]);
+
+      const users = userRes.data.data;
+      const images = imageRes.data.results;
+
+      const combined: CombinedData[] = users.map((user, index) => ({
+        id: user.id,
+        firstname: user.firstname,
+        country: user.address.country,
+        city: user.address.city,
+        imageUrl: images[index]?.url || '',
+      }));
+
+      runInAction(() => {
+        this.combinedData = combined;
+        this.loading = false;
+      });
+    } catch (error) {
+      runInAction(() => {
+        this.error = 'Ошибка загрузки данных';
+        this.loading = false;
+      });
+    }
+  };
+}
+
+export const cardDeckStore = new CardDeckStore();
