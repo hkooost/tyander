239ed7875819807d95315e6793040207562b50a3
hkooost <djzimniy@gmail.com>
2025-01-31 13:14:43 +0300

    feat: add progress icon and style classnames

--- src/components/CardDeck/index.tsx
+++ src/components/CardDeck/index.tsx
@@ -3,14 +3,17 @@ import styles from './CardDeck.module.scss';
 import Card from '../Card';
 import { observer } from 'mobx-react-lite';
 import { cardDeckStore } from '../../store/store';
+import CircularProgress from '@mui/material/CircularProgress';
 
 function CardDeck() {
   useEffect(() => {
     cardDeckStore.fetchData();
   }, []);
 
-  if (cardDeckStore.loading) return <div>Загрузка...</div>;
-  if (cardDeckStore.error) return <div>{cardDeckStore.error}</div>;
+  if (cardDeckStore.loading)
+    return <div className={styles.cardDeck_loader}><CircularProgress size="48px" thickness={8} /></div>
+  if (cardDeckStore.error)
+    return <div className={styles.cardDeck_error}>{cardDeckStore.error}</div>;
 
   return (
     <div className={styles.cardDeck}>
